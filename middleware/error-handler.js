55bb5affef8a538f75b3dcd70453c6563c1643b0
unknown <buyungalf@gmail.com>
2022-11-10 14:31:58 +0700

    error handler update

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,27 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, please try again later.",
+  };
 
-module.exports = errorHandlerMiddleware
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+  }
+
+  if (err.name === "CastError") {
+    customError.msg = `No data with id = $${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Your email '${err.keyValue.email}' is already registered.`;
+    customError.statusCode = 400;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
