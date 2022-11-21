80d026df5209ac4ce415fd13c970545f90489568
unknown <buyungalf@gmail.com>
2022-11-21 12:30:35 +0700

    additional info & swagger ui

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+//extra packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 //connect db
 const connectDB = require("./db/connect");
 
@@ -17,8 +27,22 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send("<h1>jobs api</h1><a href='/api-docs'>Documentation</a>");
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
